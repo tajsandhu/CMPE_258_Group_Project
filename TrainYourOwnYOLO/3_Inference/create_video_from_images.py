e22bea576985d3dc1112f64788e2ecb95163c73d
Michael Hsieh <mdhsieh8@gmail.com>
2021-04-19 11:30:41 -0700

    refactor: Move create video script code into functions.

--- TrainYourOwnYOLO/3_Inference/create_video_from_images.py
+++ TrainYourOwnYOLO/3_Inference/create_video_from_images.py
@@ -10,29 +10,39 @@ extension = ".png" # ".jpg"
 video_name = "../video.avi"
 WAIT_TIME_IN_MS = 1000
 
-images = [img for img in os.listdir(image_folder) if img.endswith(extension)]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+'''
+Create a video.
+'''
+create_video():
+    images = [img for img in os.listdir(image_folder) if img.endswith(extension)]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
 
-cv2.destroyAllWindows()
-video.release()
+    cv2.destroyAllWindows()
+    video.release()
 
+'''
+Display the video.
+'''
+display_video():
+    cap = cv2.VideoCapture(video_name)
 
-# Now display the video.
-cap = cv2.VideoCapture(video_name)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        cv2.waitKey(WAIT_TIME_IN_MS)
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.waitKey(WAIT_TIME_IN_MS)
-
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+    
+# Main
+create_video()
+display_video()
